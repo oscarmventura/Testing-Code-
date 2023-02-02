c0ffb599e8037c4ab11886fb79850ee61d0d788a
Oscar M <oscarmventura@icloud.com>
2023-02-02 16:18:47 -0600

    Add project files.

---
+++ testing/testing.cpp
@@ -0,0 +1,22 @@
+// testing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	long long people_on_earth { 7600000000 };
+	cout << "There are about" << people_on_earth << " people on earth" << endl;
+
+	return 0;
+
+
+
+
+
+
+
+
+}
