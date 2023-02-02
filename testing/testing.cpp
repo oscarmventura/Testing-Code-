b8d26f2e7342b3f5848d1f8c39fa714c0d7dcb29
Oscar M <oscarmventura@icloud.com>
2023-02-02 16:22:51 -0600

    testing code for floating examples

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -10,6 +10,11 @@ int main()
 	long long people_on_earth { 7600000000 };
 	cout << "There are about" << people_on_earth << " people on earth" << endl;
 
+	//Floating point types 
+
+	float car_payment{ 401.23 };
+	cout << "My car payment is " << car_payment << endl;
+
 	return 0;
 
 
