cbbbe6af89c20eac39d33b01a8abc3ae98bd1253
Oscar M <oscarmventura@icloud.com>
2023-02-02 19:24:02 -0600

    Pseudocode

--- declaring_and_using_constants/declaring_and_using_constants/declaring_and_using_constants.cpp
+++ declaring_and_using_constants/declaring_and_using_constants/declaring_and_using_constants.cpp
@@ -1,7 +1,25 @@
 // declaring_and_using_constants.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //Oscar M Ventura 
+//Service estimator program
+
+/*
+Pseudocode:
+
+Prompt the user to enter the numbers of rooms
+Display numbers of tooms
+Display price per room
+
+Display cost: (number of rooms * price per room)
+Display Tax : (number of rooms * price per room * Tax rate)
+Display total estimate: (number of rooms * price per room) + (number of rooms * price per room * Tax rate)
+Display Estimate expiration Time : 30 Days 
+
+*/
+
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
