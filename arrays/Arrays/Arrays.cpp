7f52c81fa4ec7cc5c93bd281964e8f304063cad2
Oscar M <oscarmventura@icloud.com>
2023-02-07 16:23:34 -0600

    Changing values on the array

--- arrays/Arrays/Arrays.cpp
+++ arrays/Arrays/Arrays.cpp
@@ -10,7 +10,18 @@ int main()
 	cout << "\nThe first vowel is: " << vowels[0] << endl;
 	cout << "The last vowel is: " << vowels[4] << endl;
 
-	cin >> vowels[5]; 
+	//cin >> vowels[5];   out of bounds - Will crash "error example 
+
+	//*********************************************************************
+
+	double hi_temps[]{ 90.1, 89.8, 77.5, 81.6 };
+	cout << "\nThe first high temperature is: " << hi_temps[0] << endl; //will display 90.1 original value on the array
+	
+	//Changing value
+	hi_temps[0] = 100.7;    //changing the first element in the array to 100.7
+
+	cout << "The first high temperature is now: " << hi_temps[0] << endl;
+
 
 	return 0;
 
