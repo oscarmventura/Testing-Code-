d0a5ac01868fccc6a910cdb4077994a2c586c17c
Oscar M <47688425+oscarmventura@users.noreply.github.com>
2023-02-07 15:27:54 -0600

    Add files via upload

---
+++ arrays/Arrays/Arrays.cpp
@@ -0,0 +1,13 @@
+// Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Basic arrays practices 
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
