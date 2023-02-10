97111e0e0b85e8dd42036f9a046a9ec85ae0a1b8
Oscar M <oscarmventura@icloud.com>
2023-02-09 20:31:28 -0600

    Adding a 2 Dimensional vector

--- vectors/vectors/vectors.cpp
+++ vectors/vectors/vectors.cpp
@@ -63,8 +63,32 @@ int main()
 	cout << test_scores.at(2) << endl;
 	cout << test_scores.at(3) << endl;
 	cout << test_scores.at(4) << endl;
-	
 
+	cout << "The updated size of the vector is: " << test_scores.size() << " scores in the vector." << endl;
+	
+//*******************************************************************************
+// Two Dimentional Vector 
+
+	vector <vector<int>> movie_ratings
+	{
+		{1,2,3,4},
+		{1,2,4,4},
+		{1,3,4,5}
+	};
+
+	cout << "\nHere are the movie rating for reviewer #1 using array syntax:" << endl;
+	cout << movie_ratings[0][0] << endl;
+	cout << movie_ratings[0][1] << endl;
+	cout << movie_ratings[0][2] << endl;
+	cout << movie_ratings[0][3] << endl;
+
+	cout << "\nHere are the movie rating for reviewer #1 using vector syntax:" << endl;
+	cout << movie_ratings.at(0).at(0) << endl;
+	cout << movie_ratings.at(0).at(1) << endl;
+	cout << movie_ratings.at(0).at(2) << endl;
+	cout << movie_ratings.at(0).at(3) << endl;
+	
+	
 	return 0;
 
 	
