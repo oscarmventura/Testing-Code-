c86d3f947545926227f614fd819c6ef0f6fbb285
Oscar M <oscarmventura@icloud.com>
2023-02-16 19:50:08 -0600

    operators

--- aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
+++ aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
@@ -11,10 +11,23 @@ int main()
     int num2{ 100 };
 
    //cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
-
+    //Math
     int result{ 0 };
     result = num1 + num2;
-    cout << num1 << "+" << num2 << "=" << result << endl;
+    cout << num1 << "+" << num2 << "=" << result << endl; //addition
+
+    result = num1 - num2;
+    cout << num1 << "-" << num2 << "=" << result << endl; //substaction
+
+    result = num1 * num2;
+    cout << num1 << "*" << num2 << "=" << result << endl; //multiplication
+
+    result = num1 / num2;
+    cout << num1 << "/" << num2 << "=" << result << endl; //Division 
+
+
+
+
 
     
     
