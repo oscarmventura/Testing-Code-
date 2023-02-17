3b277fac2e1cdc1f1a8bf3b055234ac7bded6ab9
Oscar M <oscarmventura@icloud.com>
2023-02-16 20:05:15 -0600

    done

--- aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
+++ aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
@@ -25,7 +25,27 @@ int main()
     result = num1 / num2;
     cout << num1 << "/" << num2 << "=" << result << endl; //Division 
 
+    result = num2 / num1;
+    cout << num1 << "/" << num2 << "=" << result << endl; // Will only output 0 because using integers...For the actual result needs to have a double to display 0.5
 
+    result = num1 % num2;
+    cout << num1 << "%" << num2 << "=" << result << endl; 
+
+    num1 = 10;
+    num2 = 3;
+    /*
+    The modulo operator, denoted by %, is an arithmetic operator. 
+    The modulo division operator produces the remainder of an integer division. 
+    Produces the remainder when x is divided by y.
+     
+    */
+    
+    result = num1 % num2;
+    cout << num1 << "%" << num2 << "=" << result << endl;
+
+    result = num1 * 10 + num2;
+    cout << 5 / 10 << endl;
+    cout << 5.0 / 10.0 << endl;
 
 
 
