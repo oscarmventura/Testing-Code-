a0f2988fc6cade7be56678af20277b6c5f85c96b
Oscar M <oscarmventura@icloud.com>
2023-02-16 19:45:58 -0600

    same output but I like it better this way

--- aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
+++ aritmetic_operators/aritmetic_operators/aritmetic_operators.cpp
@@ -10,7 +10,14 @@ int main()
     int num1{ 200 };
     int num2{ 100 };
 
-    cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+   //cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+
+    int result{ 0 };
+    result = num1 + num2;
+    cout << num1 << "+" << num2 << "=" << result << endl;
+
+    
+    
 
 
    
