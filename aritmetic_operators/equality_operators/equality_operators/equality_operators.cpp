71774cc18c1e4893bdce0f1ffbf67d675dea2551
Oscar M <oscarmventura@icloud.com>
2023-02-21 16:12:36 -0600

    using characters

--- aritmetic_operators/equality_operators/equality_operators/equality_operators.cpp
+++ aritmetic_operators/equality_operators/equality_operators/equality_operators.cpp
@@ -1,20 +1,45 @@
 // equality_operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Oscar M Ventura 
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	bool equal_result{ false };
+	bool not_equal_result{ false };
+
+	int num1{}, num2{};
+
+	cout << boolalpha;		//will display true/false instead of 1/0 for booleans
+	/*
+	cout << "Enter two integer separated by a space: ";
+	cin >> num1 >> num2;
+	equal_result = (num1 == num2);
+	not_equal_result = (num1 != num2);
+	cout << "Comparison result (equals): " << equal_result << endl;
+	cout << "Comparison result (not equals): " << not_equal_result << endl;
+	
+	*/
+
+	
+	
+	//Will apply to characters too
+
+	char char1{}, char2{};
+	cout << "Enter two characteres separated by space: ";
+	cin >> char1 >> char2;
+	equal_result = (char1 == char2);
+	not_equal_result = (char1 != char2);
+	cout << "Comparison result (equals): " << equal_result << endl;
+	cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+
+
+
+	return 0;
+
+ 
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
