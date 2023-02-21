1d18476fcd79d006d46fd69096d24955722488e6
Oscar M <oscarmventura@icloud.com>
2023-02-21 16:20:51 -0600

    testing with integers and doubles at the same time

--- aritmetic_operators/equality_operators/equality_operators/equality_operators.cpp
+++ aritmetic_operators/equality_operators/equality_operators/equality_operators.cpp
@@ -37,6 +37,8 @@ int main()
 
 	*/
 
+	/*
+	
 	double double1{}, double2{};
 	cout << "Enter two doubles separated by space: ";
 	cin >> double1 >> double2;
@@ -44,7 +46,18 @@ int main()
 	not_equal_result = (double1 != double2);
 	cout << "Comparison result (equals): " << equal_result << endl;
 	cout << "Comparison result (not equals): " << not_equal_result << endl;
+	
+	*/
 
+	
+	double double1{}, double2{};
+	cout << "Enter two integer separated by a space: ";
+	cout << "Enter two doubles separated by space: ";
+	cin >> num1 >> double1;
+	equal_result = (num1 == double1);
+	not_equal_result = (num1 != double1);
+	cout << "Comparison result (equals): " << equal_result << endl;
+	cout << "Comparison result (not equals): " << not_equal_result << endl;
 
 
 	return 0;
