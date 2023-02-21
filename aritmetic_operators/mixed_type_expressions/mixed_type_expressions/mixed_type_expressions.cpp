387adf0d93c41d21eb69fdc8a8131c721b142b15
Oscar M <oscarmventura@icloud.com>
2023-02-20 21:25:56 -0600

    resetting the default setting of VS

--- aritmetic_operators/mixed_type_expressions/mixed_type_expressions/mixed_type_expressions.cpp
+++ aritmetic_operators/mixed_type_expressions/mixed_type_expressions/mixed_type_expressions.cpp
@@ -1,20 +1,27 @@
 // mixed_type_expressions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Oscar M Ventura  
 
+/*
+Ask the user to enter 3 integers
+Calculate the sum of the integers then
+calculate the average of the 3 integers.
+
+Display each of them, aslo the average
+*/
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num1{ 0 };
+	int num2{ 0 };
+	int num3{ 0 };
+
+  
+
+
+	int sum_of = 0;
+
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
