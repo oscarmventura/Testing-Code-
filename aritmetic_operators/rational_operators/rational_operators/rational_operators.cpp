a6b6eba5bfb53e0a9ce69c08183c8bd6631ca329
Oscar M <oscarmventura@icloud.com>
2023-02-21 20:16:44 -0600

    completed

--- aritmetic_operators/rational_operators/rational_operators/rational_operators.cpp
+++ aritmetic_operators/rational_operators/rational_operators/rational_operators.cpp
@@ -1,20 +1,44 @@
 // rational_operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Oscar M Ventura
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num1{}, num2{};
+
+	cout << boolalpha;
+	// Example 1
+	/*
+
+	cout << "Enter 2 integers separated by a space: ";
+	cin >> num1 >> num2;
+
+	cout << num1 << " > " << num2 << " : " << (num1 > num2) << endl;
+	cout << num1 << " >= " << num2 << " : " << (num1 >= num2) << endl;
+	cout << num1 << " < " << num2 << " : " << (num1 < num2) << endl;
+	cout << num1 << " <= " << num2 << " : " << (num1 <= num2) << endl;
+
+	*/
+
+	//Example 2
+	const int lower{ 10 };
+	const int higher{ 20 };
+
+	cout << "\nEnter an integer that is greater than " << lower << " : ";
+	cin >> num1;
+	cout << num1 << " > " << lower << " is " << (num1 > lower) << endl;
+
+	cout << "\nEnter a integer that is less or equal than " << higher << " : ";
+	cin >> num1;
+	cout << num1 << " <= " << higher << " is " << (num1 <= higher) << endl;
+
+	cout << endl;
+
+
+	 
+	return 0;
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
