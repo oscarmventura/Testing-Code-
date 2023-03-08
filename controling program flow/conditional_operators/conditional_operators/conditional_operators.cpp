70e18daecbc9b6fa0a9130cc0ace394fafe5434a
Oscar M <oscarmventura@icloud.com>
2023-03-07 19:02:08 -0600

    Example 2

--- controling program flow/conditional_operators/conditional_operators/conditional_operators.cpp
+++ controling program flow/conditional_operators/conditional_operators/conditional_operators.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main()
 {
+	/*
+	
 	int num{};
 
 	cout << "Enter and integer: ";
@@ -20,8 +22,32 @@ int main()
 
 	cout << num << " is " << ((num % 2 == 0) ? "even" : "odd") << endl;
 
+	*/
+
+	//Example 2
+
+	int num1{}, num2{};
+
+	cout << " Enter two integers separated by space: ";
+	cin >> num1 >> num2;
+
+	if (num1 != num2) {
+		cout << "Largest " << ((num1 > num2) ? num1 : num2) << endl;
+		cout << "Smallest " << ((num1 < num2) ? num1 : num2) << endl;
+
+
+	}
+	else {
+
+		cout << "The numbers are the same!" << endl;
+	}
+		
+	
+	
+		
+
+	
+
 	return 0;
-
-
 }
 
