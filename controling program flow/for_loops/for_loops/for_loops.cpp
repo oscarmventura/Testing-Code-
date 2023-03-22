112e88a58a19effba341e03da04c3ffa866f1836
Oscar M <oscarmventura@icloud.com>
2023-03-21 19:42:58 -0500

    im tired

--- controling program flow/for_loops/for_loops/for_loops.cpp
+++ controling program flow/for_loops/for_loops/for_loops.cpp
@@ -18,8 +18,23 @@ int main()
 	
 	*/
 	
+	/*
 	for (int i{ 10 }; i > 0; --i)
 		cout << i << endl;
+	*/
+
+	/*
+	for (int i{ 10 }; i <= 100; i += 10) {
+		if (i % 15 == 0)
+		cout << i << endl;
+		
+	}	
+	*/
+
+	for (int i{ 1 }, j{ 5 }; i <= 5; ++i, ++j)
+		cout << i << " + " << j << " = " << (i+j) << endl;
+		
+
 
 
 	return 0;
