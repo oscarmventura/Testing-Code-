49a8a7873179b74ddc168a9413dd41b620d9bfef
Oscar M <oscarmventura@icloud.com>
2023-03-21 19:32:34 -0500

    for loop examples

---
+++ controling program flow/for_loop/for_loop/for_loop.cpp
@@ -0,0 +1,16 @@
+// for_loop.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i{1}; ++i;)
+		cout << i;
+
+	return 0;
+	
+	
+}
---
+++ controling program flow/for_loops/for_loops/for_loops.cpp
@@ -0,0 +1,27 @@
+// for_loops.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	for (int i{ 1 }; i <= 10; ++i)
+		cout << i << endl;
+	
+	*/
+	
+	/*
+	for (int i{ 1 }; i <= 10; i+=2)
+		cout << i << endl;
+	
+	*/
+	
+	for (int i{ 10 }; i > 0; --i)
+		cout << i << endl;
+
+
+	return 0;
+}
+
